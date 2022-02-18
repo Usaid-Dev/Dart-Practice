ed2741254c6c2d81e5f1bd0e2a73fd752ebb1e99
Usaid Asif <usaidasif12@gmail.com>
2022-02-18 17:42:18 +0500

    Learn Variables

---
+++ variables.dart
@@ -0,0 +1,16 @@
+void main() {
+  print("First Assignment");
+  print("----------------");
+
+  List<String> students = ["hamza", "ali", "saad", "haris"];
+  int total = 4;
+  print("Total Students in class=");
+  print(total);
+  print("Students Name=");
+  print(students);
+  print(students[3] + " is absent");
+  bool harisisnotintheclass = true;
+  print(harisisnotintheclass);
+  String lecture = "lecture is going to start";
+  print(lecture);
+}
