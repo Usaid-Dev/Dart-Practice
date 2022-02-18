3812dd7d5540db870d90b4b3fc184ad04870b2aa
Usaid Asif <67046451+Usaid-Dev@users.noreply.github.com>
2022-02-18 17:48:57 +0500

    Learn List & Map

---
+++ List.dart
@@ -0,0 +1,35 @@
+void main() {
+  List<Map<String, String>> Instagramposts = [
+    {
+      " User id ": " Usaid Asif ",
+      " Post ":
+          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fpixabay.com%2Fimages%2Fsearch%2Fcat%2F&psig=AOvVaw2Irz5k-BJErS2xb8rdlFJq&ust=1642077740845000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCOj_graerPUCFQAAAAAdAAAAABAD",
+      " Post Caption ": "Cute cats"
+    },
+    {
+      " User id ": " Ali Ahmed ",
+      " Post ":
+          "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.akc.org%2Fdog-breeds%2Fbulldog%2F&psig=AOvVaw2_pZmIwl3x-3oo8SahbH0u&ust=1642077872942000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCMi0ofaerPUCFQAAAAAdAAAAABAD",
+      " Post Caption ": "I love this Bulldog"
+    },
+    {
+      " User id ": " Anas ",
+      " Post ":
+          "https://www.google.com/search?q=bmw+i8+interior&tbm=isch&chips=q:bmw+i8+interior,g_1:red:OyovRH6d07k%3D&hl=en&sa=X&ved=2ahUKEwix8aCxn6z1AhUD7xoKHdI6C-wQ4lYoBHoECAEQIg&biw=1226&bih=577#imgrc=udOdfkQvKjtLuM",
+      " Post Caption ": "I love this Bmw i8"
+    },
+    {
+      " User id ": " Ahmed ",
+      " Post ":
+          "https://www.google.com/search?q=maldives&tbm=isch&ved=2ahUKEwj6j-iyn6z1AhUNaRoKHSsUCJQQ2-cCegQIABAA&oq=maldi&gs_lcp=CgNpbWcQARgAMgcIABCxAxBDMgQIABBDMgQIABBDMgQIABBDMgQIABBDMgQIABBDMgsIABCABBCxAxCDATIICAAQgAQQsQMyBQgAEIAEMgUIABCABDoHCCMQ7wMQJzoKCCMQ7wMQ6gIQJ1D8CVi-HmDOJ2gBcAB4AIABzwGIAfsIkgEFMC41LjGYAQCgAQGqAQtnd3Mtd2l6LWltZ7ABCsABAQ&sclient=img&ei=tc3eYbrhC43SaauooKAJ&bih=577&biw=1226&hl=en#imgrc=DFatGQLccVrTyM",
+      " Post Caption ": "This is my Dream place"
+    },
+    {
+      " User id ": " Hamza ",
+      " Post ":
+          "https://www.google.com/search?q=Lovely+weather&tbm=isch&ved=2ahUKEwiPyeaOoKz1AhUQ0oUKHeC-AB0Q2-cCegQIABAA&oq=Lovely+weather&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQyBQgAEIAEMgUIABCABDIFCAAQgAQ6BwgjEO8DECc6CggAELEDEIMBEEM6BAgAEEM6BwgAELEDEEM6CggjEO8DEOoCECc6CAgAEIAEELEDOgsIABCABBCxAxCDAVD3Blj6ImC9JGgBcAB4AIABxgGIAccVkgEEMC4xNZgBAKABAaoBC2d3cy13aXotaW1nsAEKwAEB&sclient=img&ei=ds7eYY-2BpCklwTg_YLoAQ&bih=577&biw=1226&hl=en#imgrc=wU9EwIh6M0I_tM",
+      " Post Caption ": "Lovely Weather"
+    },
+  ];
+  print(Instagramposts[4]);
+}
